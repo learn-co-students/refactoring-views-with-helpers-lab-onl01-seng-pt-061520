800e70a609bbabea7137b99570d17333ff8c565c
mjaber <abujaber44@gmail.com>
2020-11-10 18:04:33 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,10 @@
 module ArtistsHelper
+    def display_artist(name)
+        @artist = Artist.find_by name:(name)
+        if @artist != nil
+        link_to @artist.name, artist_path(@artist)
+        else
+        link_to "Add Artist", edit_song_path(@song)
+        end 
+    end 
 end
